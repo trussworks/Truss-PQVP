8b09166dd278403025fe2fa331cca5d613eb0c2f
Michael Kania <dynamike@truss.works>
2017-02-16 13:38:14 -0800

    shrink instance size and bump count to 3 for faster deployments

--- terraform/aws-us-west-2/asg.tf
+++ terraform/aws-us-west-2/asg.tf
@@ -6,13 +6,13 @@ module "asg" {
   security_group_ids          = ["${aws_security_group.asg-sg.id}"]
   availability_zones          = "${module.vpc.availability_zones}"
   subnet_ids                  = "${module.vpc.internal_subnets}"
-  instance_type               = "t2.small"
+  instance_type               = "t2.micro"
   environment                 = "${var.environment}"
   key_name                    = "dynamike"
   associate_public_ip_address = false
-  min_size                    = 2
-  max_size                    = 2
-  desired_capacity            = 2
+  min_size                    = 3
+  max_size                    = 3
+  desired_capacity            = 3
   iam_instance_profile        = "${module.pqvp-demo.ecs_instance_profile_name}"
 
   user_data = <<EOF

608928585317e7f7b579640a40a42f263539739d
Emily Chen <emily@truss.works>
2017-02-20 13:32:35 -0500

    Basic app up and running

---
+++ client/dev-server.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+const isDev = (process.env.NODE_ENV !== 'production');
+
+if (isDev) {
+  const config = require('./webpack.config');
+  const WebpackDevServer = require('webpack-dev-server');
+
+  new WebpackDevServer(webpack(config), {
+    publicPath: "/",
+    hot: true,
+    proxy: { "/api/*" : "http://localhost:8080" }
+  }).listen(3001, 'localhost', function (err, result) {
+    if (err) { console.log(err) }
+    console.log('Listening at localhost:3001');
+  });
+}

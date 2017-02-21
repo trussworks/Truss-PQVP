9e0f45a238e16f0b0b1e1b946bf2a87eae1b6e7f
Emily Chen <emily@truss.works>
2017-02-21 12:13:36 -0500

    Added a Header that displays across routes

---
+++ client/src/landingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { remote: 'Have Not Talked To Server' };
+    const resp = fetch('/hello/PQVP');
+    resp.then(response => response.text()).then((text) => {
+      this.setState({ remote: `Server says: ${text}` });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>my Root is on Fire!</h1>
+        <p>{ this.state.remote }</p>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;

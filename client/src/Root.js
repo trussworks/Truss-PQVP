782c1d5221b1972fac8300e0002f6c0b2c285f6f
MacRae Linton <macrae.linton@gmail.com>
2017-02-20 16:55:47 -0800

    Tracer Round connecting client to server
    * switched to class notation for Root
    * added proxy to dev-server (will have to be updated for actual
    endpoints)
    * fixex typo

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export const Root = () => (
-  <div>
-    <h1>my Root is on Fire!</h1>
-  </div>
-);
+export class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    const resp = fetch('/hello/there');
+    resp.then(response => response.text()).then((text) => {
+      console.log(text);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>my Root is on Fire!</h1>
+      </div>
+    );
+  }
+}
 
 export default Root;

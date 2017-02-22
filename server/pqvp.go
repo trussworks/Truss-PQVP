0e41889946af4fc244e39fddcdf1b597b2461172
Mojo Talantikite <mojo.talantikite@gmail.com>
2017-02-22 15:48:58 -0500

    Use latest goji, support for 1.7 context

--- server/pqvp.go
+++ server/pqvp.go
@@ -3,12 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"net"
 	"net/http"
 
-	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
+	"goji.io"
+	"goji.io/pat"
 )
 
 func main() {
@@ -18,20 +16,18 @@ func main() {
 	port := flag.String("port", ":80", "the `port` to listen on.")
 	flag.Parse()
 
-	listener, err := net.Listen("tcp", *port)
-	if err != nil {
-		log.Fatal("Could not bind to port")
-	}
+	mux := goji.NewMux()
 
-	goji.Get("/hello/:name", hello)
-	goji.Get("/", IndexHandler(entry))
-	goji.Handle("/:file.:ext", http.FileServer(http.Dir(*static)))
-	goji.ServeListener(listener)
+	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+	mux.HandleFunc(pat.Get("/"), IndexHandler(entry))
+	mux.Handle(pat.Get("/:file.:ext"), http.FileServer(http.Dir(*static)))
+
+	http.ListenAndServe(*port, mux)
 }
 
 // Hello takes a request along with params and responds with hello :name
-func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, %s!\n", c.URLParams["name"])
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, %s!\n", pat.Param(r, "name"))
 }
 
 // IndexHandler serves up our index.html

8f35c7e767c3a30b940effe8a672b04e6b2645c7
Michael Kania <dynamike@truss.works>
2017-02-22 12:42:25 -0800

    update entrypoint to run migrations

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,6 +4,9 @@ set -eux -o pipefail
 # start postgres and create pvqp user
 /etc/init.d/postgresql start
 
-su postgres -c "psql < /go/src/app/server/sql/init.sql"
+# run migrations
+su postgres -c "createdb pqvp"
+su postgres -c "psql -c \"ALTER USER postgres PASSWORD '';\""
+/go/bin/migrate -url "postgres://postgres@localhost:5432/pqvp?sslmode=disable" -path /go/src/app/server/sql up
 
 /go/src/app/server/server -entry /go/src/app/client/dist/index.html -static /go/src/app/client/dist/

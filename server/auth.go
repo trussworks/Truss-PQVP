aeace7f290968b1a283a9bac2151b4845d55d818
Mojo Talantikite <mojo.talantikite@gmail.com>
2017-02-22 20:52:26 -0500

    Add middleware to use jwt auth

--- server/auth.go
+++ server/auth.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"net/http"
+
 	jwt "github.com/dgrijalva/jwt-go"
+	request "github.com/dgrijalva/jwt-go/request"
 	"go.uber.org/zap"
 )
 
@@ -10,13 +14,13 @@ type customClaims struct {
 	jwt.StandardClaims
 }
 
+// A signing key should typically be a private key that is stored
+// in a secure secrets management system. This would change in a
+// non-prototype, production system.
+var signingKey = []byte("truss-pqvp-demo")
+
 // CreateJwt takes a User and returns a jwt token that has custom claims set.
 func CreateJwt(u User) string {
-	// A signing key should typically be a private key that is stored
-	// in a secure secrets management system. This would change in a
-	// non-prototype, production system
-	signingKey := []byte("truss-pqvp-demo")
-
 	claims := customClaims{
 		u.Email,
 		jwt.StandardClaims{
@@ -32,4 +36,17 @@ func CreateJwt(u User) string {
 		)
 	}
 	return ss
+}
+
+// Allowed takes a request and verifies if the user is allowed to access
+// the requested resource
+func Allowed(r *http.Request) (jwt.MapClaims, error) {
+	token, err := request.ParseFromRequestWithClaims(r, request.AuthorizationHeaderExtractor, customClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return signingKey, nil
+	})
+	if err == nil && token.Valid {
+		return token.Claims.(jwt.MapClaims), nil
+	}
+
+	return nil, errors.New("user is not allowed")
 }

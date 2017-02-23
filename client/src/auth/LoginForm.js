98d3a2d8314c664da9bcef4da2f52f290961c9a0
Emily Chen <emily@truss.works>
2017-02-23 15:13:12 -0800

    Improving accessibility on forms

--- client/src/auth/LoginForm.js
+++ client/src/auth/LoginForm.js
@@ -3,34 +3,41 @@ import { Field, reduxForm } from 'redux-form';
 import authValidation from './authValidation';
 import { AuthField } from '../auth/AuthField';
 
-const LoginForm = ({ handleSubmit, submitting, valid }) => (
-  <form onSubmit={handleSubmit}>
-    <div>
-      <Field
-        component={AuthField}
-        label="Email"
-        name="email"
-        placeholder="Email address"
-        type="email"
-      />
-    </div>
-    <div>
-      <Field
-        component={AuthField}
-        label="Password"
-        name="password"
-        placeholder="Password"
-        type="password"
-      />
-    </div>
-    <button type="submit" disabled={submitting || !valid}>Sign In</button>
+const LoginForm = ({ handleSubmit, submitting, switchForm, valid }) => (
+  <form className="usa-form" onSubmit={handleSubmit}>
+    <fieldset>
+      <legend className="usa-drop_text">Sign in</legend>
+      <span>
+        or <a href="" onClick={switchForm}>create an account</a>
+      </span>
+      <div>
+        <Field
+          component={AuthField}
+          label="Email"
+          name="email"
+          placeholder="Email address"
+          type="email"
+        />
+      </div>
+      <div>
+        <Field
+          component={AuthField}
+          label="Password"
+          name="password"
+          placeholder="Password"
+          type="password"
+        />
+      </div>
+      <input type="submit" disabled={submitting || !valid} value="Sign in" />
+    </fieldset>
   </form>
   );
 
 LoginForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
-  submitting: PropTypes.bool,
-  valid: PropTypes.bool,
+  submitting: PropTypes.bool.isRequired,
+  switchForm: PropTypes.func.isRequired,
+  valid: PropTypes.bool.isRequired,
 };
 
 export default reduxForm({

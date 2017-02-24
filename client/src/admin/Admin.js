c0fcf3b45e4787d86e2a669d85c703ec8bf04ba6
Emily Chen <emily@truss.works>
2017-02-23 18:38:34 -0800

    Displaying map with layer

--- client/src/admin/Admin.js
+++ client/src/admin/Admin.js
@@ -1,40 +1,44 @@
 import React from 'react';
 import { layer, source, Map, View } from 'openlayers';
-import ReactDom, { render } from 'react-dom';
 
 class Admin extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      url: 'https://igems.doi.gov/arcgis/rest/services/igems_haz/MapServer/10?f=pjson',
+      url: 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/' +
+          'Specialty/ESRI_StateCityHighway_USA/MapServer',
     };
   }
-  render() {
-    var layers = [
+  componentDidMount() {
+    const layers = [
       new layer.Tile({
-        source: new source.OSM()
+        source: new source.OSM(),
       }),
       new layer.Tile({
         extent: [-13884991, 2870341, -7455066, 6338219],
         source: new source.TileArcGISRest({
           url: this.state.url,
-        })
-      })
+        }),
+      }),
     ];
 
-    var map = new Map({
-      layers: layers,
+    const map = new Map({
+      layers,
       target: 'map',
       view: new View({
         center: [-10997148, 4569099],
-        zoom: 4
-      })
+        zoom: 4,
+      }),
     });
 
+    map.setTarget(this.mapView);
+  }
+  render() {
     return (
       <div className="container">
         <h1>Admin</h1>
+        <div ref={(input) => { this.mapView = input; }} />
       </div>
     );
   }

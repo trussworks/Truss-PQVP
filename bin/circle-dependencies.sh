e33203b2feda1576605ce9a9ad31b22755d50cdb
Mark Ferlatte <ferlatte@cryptio.net>
2017-02-25 15:20:47 -0800

    Try to fix the busted cache.

--- bin/circle-dependencies.sh
+++ bin/circle-dependencies.sh
@@ -2,16 +2,9 @@
 
 set -eux -o pipefail
 
-# Pull down and install terraform to run tests
-sudo wget https://releases.hashicorp.com/terraform/0.8.6/terraform_0.8.6_linux_amd64.zip -O /usr/local/bin/terraform.zip
-pushd /usr/local/bin
-sudo unzip terraform.zip
-popd
-
-# Clone ecs-deploy so we can push task updates to ECS once tests past
-git clone https://github.com/silinternational/ecs-deploy.git
 
 # Get go environment setup
+rm -rf ~/.go_workspace/src/github.com/mattes
 go get github.com/Masterminds/glide
 go get github.com/mattes/migrate
 pushd server
@@ -28,6 +21,15 @@ popd
 # swagger doc generation dependencies
 yarn global add bootprint
 yarn global add bootprint-openapi
+
+# Pull down and install terraform to run tests
+sudo wget https://releases.hashicorp.com/terraform/0.8.6/terraform_0.8.6_linux_amd64.zip -O /usr/local/bin/terraform.zip
+pushd /usr/local/bin
+sudo unzip terraform.zip
+popd
+
+# Clone ecs-deploy so we can push task updates to ECS once tests past
+git clone https://github.com/silinternational/ecs-deploy.git
 
 # Pull down Sauce Labs Connect
 # FIXME: disabled due to Sauce just not working. --mark

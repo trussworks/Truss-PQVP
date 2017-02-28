54f739e884fe039716ab9d807f685ad6fa9ec157
Mojo Talantikite <mojo.talantikite@gmail.com>
2017-02-27 22:45:43 -0500

    Add logging to middleware

--- server/src/pqvp/middleware.go
+++ server/src/pqvp/middleware.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 func authMiddleware(i http.Handler) http.Handler {
@@ -12,6 +14,9 @@ func authMiddleware(i http.Handler) http.Handler {
 		if err != nil {
 			w.WriteHeader(http.StatusForbidden)
 			fmt.Fprintf(w, "user is not allowed")
+			logger.Error("user is not allowed",
+				zap.Error(err),
+			)
 			return
 		}
 		ctx := context.WithValue(r.Context(), userKey, User{

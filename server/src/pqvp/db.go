21d1617a97919fa1d30b21e54dcbc40fe5650097
Mark Ferlatte <ferlatte@cryptio.net>
2017-02-28 16:52:35 -0800

    Add GetProfile

--- server/src/pqvp/db.go
+++ server/src/pqvp/db.go
@@ -36,6 +36,48 @@ func (pg *Postgres) CreateUser(u User) error {
 	return nil
 }
 
+func (pg *Postgres) FetchProfile(email string) (Profile, error) {
+	row := pg.QueryRow(`
+SELECT profiles.id, profiles.phone
+FROM profiles, users
+WHERE profiles.user_id = users.id AND users.email = $1`, email)
+	var phone string
+	var profile_id int32
+	err := row.Scan(&profile_id, &phone)
+	if err != nil {
+		return Profile{}, err
+	}
+	addresses := make([]ProfileAddress, 0)
+	profile := Profile{phone, addresses}
+	rows, err := pg.Query(`
+SELECT addresses.address,
+ST_X(addresses.point) AS longitude,
+ST_Y(addresses.point) AS latitude
+FROM addresses, profiles, users
+WHERE addresses.profile_id = profiles.id
+AND profiles.user_id = users.id
+AND users.email=$1;`, email)
+	if err != nil {
+		return Profile{}, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var address string
+		var longitude, latitude float64
+		err = rows.Scan(&address, &longitude, &latitude)
+		if err != nil {
+			return Profile{}, err
+		}
+		addy := ProfileAddress{address, latitude, longitude}
+		profile.Addresses = append(profile.Addresses, addy)
+	}
+	err = rows.Err()
+	if err != nil {
+		return Profile{}, err
+	}
+	return profile, nil
+}
+
 // Close implements io.Closer
 func (pg *Postgres) Close() error {
 	return pg.DB.Close()
@@ -48,6 +90,7 @@ type Postgres struct{ *sql.DB }
 type Datastore interface {
 	LoginUser(User) bool
 	CreateUser(User) error
+	FetchProfile(string) (Profile, error)
 	io.Closer
 }
 

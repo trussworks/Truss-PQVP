d923c17422004571533b8ef251bb69f258799da8
MacRae Linton <macrae.linton@gmail.com>
2017-02-28 16:26:45 -0800

    Silence eslint warning about unused import

--- client/src/Root.js
+++ client/src/Root.js
@@ -7,7 +7,7 @@ import { Router, Route, browserHistory } from 'react-router';
 import App from './app/App';
 import LandingPage from './landingPage/LandingPage';
 import ProfileContainer from './profile/ProfileContainer';
-import requireAuth from './auth/requireAuth';
+// import requireAuth from './auth/requireAuth';
 import { rootReducer } from './rootReducer';
 import AdminPage from './admin/AdminPage';
 

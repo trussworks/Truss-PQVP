0d85da763cbf962db29bb6879bac6d27a9cdfb68
Mark Ferlatte <ferlatte@cryptio.net>
2017-03-02 10:50:51 -0800

    Add sent_alerts table.

---
+++ server/sql/1488479363_add_sent_alerts_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sent_alerts (
+       id SERIAL PRIMARY KEY,
+       message TEXT NOT NULL,
+       sent_sms INTEGER NOT NULL,
+       sent_email INTEGER NOT NULL,
+       sent_people INTEGER NOT NULL,
+       geo GEOMETRY(GEOMETRY, 4326) NOT NULL,
+       sender TEXT NOT NULL
+);
+GRANT ALL PRIVILEGES ON TABLE sent_alerts TO pqvp;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO pqvp;

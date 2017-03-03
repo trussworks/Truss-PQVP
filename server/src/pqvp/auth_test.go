937218d0efbc9f8d4eef9a155dc9059245b0cd1a
Mojo Talantikite <mojo.talantikite@gmail.com>
2017-03-03 11:40:00 -0500

    Test our jwt handling

--- server/src/pqvp/auth_test.go
+++ server/src/pqvp/auth_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"testing"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -26,4 +29,35 @@ func TestCreateJwt(t *testing.T) {
 	}
 
 	t.Errorf("Token was invalid.")
+}
+
+func TestBadCreateJwt(t *testing.T) {
+	_, err := CreateJwt(User{})
+	if err != nil {
+		t.Errorf("Should not have received JWT back.")
+	}
+}
+
+func TestAllowed(t *testing.T) {
+	user := User{"miles@truss.works", "blueingreen"}
+	jwtToken, err := CreateJwt(user)
+	if err != nil {
+		t.Errorf("Could not create JWT")
+	}
+	u, _ := json.Marshal(user)
+
+	req, err := http.NewRequest("GET", "/", bytes.NewBuffer(u))
+	if err != nil {
+		t.Errorf("Could not creqte the request")
+	}
+	req.Header.Set("Authorization", jwtToken)
+
+	claims, err := Allowed(req)
+	if err != nil {
+		t.Errorf("Could not auth the token")
+	}
+
+	if claims.Email != user.Email {
+		t.Errorf("JWT claims not properly set")
+	}
 }

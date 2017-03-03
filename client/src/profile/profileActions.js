2cb67a62518ba8eff7e021fac0fdafed497d61a1
MacRae Linton <macrae.linton@gmail.com>
2017-03-03 02:42:37 -0800

    One at a time to the server.
    Rather than having server returns write over current state, we are going
    to queue up all our requests to the server one at a time.

--- client/src/profile/profileActions.js
+++ client/src/profile/profileActions.js
@@ -3,6 +3,9 @@ import { SAVE_PROFILE } from '../constants/actionTypes';
 import { displayAlert, dismissAlert } from '../app/appActions';
 import { logOutUser } from '../auth/authActions';
 
+let isUpdating = false;
+let nextProfile = null;
+
 export function saveProfile(profile) {
   return { type: SAVE_PROFILE, userInfo: profile };
 }
@@ -10,6 +13,16 @@ export function saveProfile(profile) {
 const PROFILE_URL = '/api/profile/';
 
 export function updateProfile(authToken, newProfile) {
+  console.log('BEGIN');
+  if (isUpdating) {
+    // If we are in the middle of a request, just replace nextProfile with newProfile
+    nextProfile = newProfile;
+    console.log('saving profile999999');
+    return saveProfile(newProfile);
+  }
+
+  // If we aren't requesting, then we request.
+  isUpdating = true;
   const headers = new Headers();
   headers.append('Authorization', `Bearer ${authToken}`);
 
@@ -21,23 +34,49 @@ export function updateProfile(authToken, newProfile) {
 
   return (dispatch) => {
     dispatch(saveProfile(newProfile));
-    fetch(PROFILE_URL, fetchInit)
-    .then(actionHelpers.checkStatus)
-    .then(actionHelpers.parseJSON)
-    .then((profile) => {
-      dispatch(saveProfile(profile));
-      dispatch(dismissAlert());
-    })
-    .catch((error) => {
-      if (error.response.status === 403) {
-        // Forbidden means our auth didn't auth
-        dispatch(logOutUser());
-        dispatch(displayAlert('usa-alert-error', 'Error Loading Profile', 'We were unable to update your profile. Please login and try again.'));
-      } else {
-        dispatch(displayAlert('usa-alert-error', 'Error Loading Profile', 'We were unable to load your profile. Please refresh the page and try again.'));
-        console.error('getProfile Error: ', error);
-      }
-    });
+    console.log('newporf should be: ', newProfile);
+    console.log('sending');
+    window.setTimeout(() => {
+      console.log('snet');
+      fetch(PROFILE_URL, fetchInit)
+      .then(actionHelpers.checkStatus)
+      .then(actionHelpers.parseJSON)
+      .then((profile) => {
+        console.log('got back', profile);
+        isUpdating = false;
+        dispatch(dismissAlert());
+        if (nextProfile) {
+          const theProfile = nextProfile;
+          nextProfile = null;
+          // If there was a new profile waiting to be sent, send it.
+          console.log('recurse');
+          dispatch(updateProfile(authToken, theProfile));
+        } else {
+          console.log('saving profile!');
+          dispatch(saveProfile(profile));
+        }
+        console.log('back');
+      })
+      .catch((error) => {
+        console.log('errororred');
+        isUpdating = false;
+        if (error.response.status === 403) {
+          // Forbidden means our auth didn't auth
+          console.log(error);
+          dispatch(logOutUser());
+          dispatch(displayAlert('usa-alert-error', 'Error Loading Profile', 'We were unable to update your profile. Please login and try again.'));
+        } else if (nextProfile) {
+          console.error('getProfile Error, retrying: ', error);
+          const theProfile = nextProfile;
+          nextProfile = null;
+          // If there was a new profile waiting to be sent, send it.
+          dispatch(updateProfile(theProfile, nextProfile));
+        } else {
+          dispatch(displayAlert('usa-alert-error', 'Error Loading Profile', 'We were unable to load your profile. Please refresh the page and try again.'));
+          console.error('getProfile Final Error: ', error);
+        }
+      });
+    }, 3000);
   };
 }
 
@@ -61,6 +100,7 @@ export function getProfile(authToken) {
     dispatch(dismissAlert());
   })
   .catch((error) => {
+    console.log(error);
     console.log(error.response.status);
     console.log(error.response);
     if (error.response.status === 403) {

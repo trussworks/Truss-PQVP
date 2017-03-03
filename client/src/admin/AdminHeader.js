d65310e502631749f84f7495fa76754ec2d823b3
MacRae Linton <macrae.linton@gmail.com>
2017-03-02 16:20:10 -0800

    Put in some explainatory callout text

--- client/src/admin/AdminHeader.js
+++ client/src/admin/AdminHeader.js
@@ -3,6 +3,10 @@ import React from 'react';
 const AdminHeader = () => (
   <div>
     <h1 className="text--center text__margin--70">Send New Notification</h1>
+    <div className="container--blue">
+      Use this page to send an alert to citizens regsitered in one of the regions below.
+      These regions are provided by the <a href="https://igems.doi.gov">IGEMS</a>.
+    </div>
   </div>
 );
 

29f330bc488d64ac7651684a9db697cbf59f4b89
MacRae Linton <macrae.linton@gmail.com>
2017-03-03 09:00:07 -0800

    icon should have absolute path

--- client/src/header/Logo.js
+++ client/src/header/Logo.js
@@ -4,7 +4,7 @@ const Logo = () => (
   <div className="usa-navbar container--logo">
     <button className="usa-menu-btn">Menu</button>
     <div className="usa-logo container--logo-image" id="logo">
-      <img className="logo--ca usa-media_block-img" src="dist/public/img/logo-CAState.png" alt="State of California Seal" />
+      <img className="logo--ca usa-media_block-img" src="/dist/public/img/logo-CAState.png" alt="State of California Seal" />
       <div className="container--logo-text">
         <em className="usa-logo-text">
           <a href="" title="Home" aria-label="Home">Emergency <br />Alert App</a>
